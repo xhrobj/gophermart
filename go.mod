4c12467412fed69238ba715f536ab81f4f412534
M <m1@mishe1.ru>
2026-04-17 16:45:37 +0300

    Добавлена postgres-реализация UserRepository

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.26
 require (
 	github.com/golang-jwt/jwt/v5 v5.3.1
 	github.com/golang-migrate/migrate/v4 v4.19.1
+	github.com/jackc/pgerrcode v0.0.0-20250907135507-afb5586c32a6
 	github.com/jackc/pgx/v5 v5.9.1
 	github.com/stretchr/testify v1.11.1
 	go.uber.org/zap v1.27.1
--- internal/repository/user.go
+++ internal/repository/user.go
@@ -2,8 +2,12 @@ package repository
 
 import (
 	"context"
+	"database/sql"
 	"errors"
+	"fmt"
 
+	"github.com/jackc/pgerrcode"
+	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/xhrobj/gophermart/internal/model"
 )
 
@@ -19,4 +23,59 @@ type UserRepository interface {
 
 	// FindByLogin возвращает пользователя по логину.
 	FindByLogin(ctx context.Context, login string) (model.User, error)
+}
+
+type PostgresUserRepository struct {
+	db *sql.DB
+}
+
+func NewPostgresUserRepository(db *sql.DB) *PostgresUserRepository {
+	return &PostgresUserRepository{
+		db: db,
+	}
+}
+
+func (r *PostgresUserRepository) Create(ctx context.Context, login, passwordHash string) (model.User, error) {
+	const query = "INSERT INTO users (login, password_hash) VALUES ($1, $2) RETURNING id, login, password_hash, created_at"
+
+	var user model.User
+
+	err := r.db.QueryRowContext(ctx, query, login, passwordHash).Scan(
+		&user.ID,
+		&user.Login,
+		&user.PasswordHash,
+		&user.CreatedAt,
+	)
+	if err != nil {
+		var pgErr *pgconn.PgError
+		if errors.As(err, &pgErr) && pgErr.Code == pgerrcode.UniqueViolation {
+			return model.User{}, ErrUserAlreadyExists
+		}
+
+		return model.User{}, fmt.Errorf("create user: %w", err)
+	}
+
+	return user, nil
+}
+
+func (r *PostgresUserRepository) FindByLogin(ctx context.Context, login string) (model.User, error) {
+	const query = "SELECT id, login, password_hash, created_at FROM users WHERE login = $1"
+
+	var user model.User
+
+	err := r.db.QueryRowContext(ctx, query, login).Scan(
+		&user.ID,
+		&user.Login,
+		&user.PasswordHash,
+		&user.CreatedAt,
+	)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return model.User{}, ErrUserNotFound
+		}
+
+		return model.User{}, fmt.Errorf("find user by login: %w", err)
+	}
+
+	return user, nil
 }
